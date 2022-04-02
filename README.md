e641850177169c710421a2b00347247f95816cbb
scarapia <s.carapia@hotmail.com>
2022-04-01 22:56:40 -0600

    15_columns_share_post

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-git branch 14_rows_and_columns_2 && git checkout 14_rows_and_columns_2 && git add . && git commit -m "14_rows_and_columns_2" && git push --set-upstream origin 14_rows_and_columns_2
+git branch 15_columns_share_post && git checkout 15_columns_share_post && git add . && git commit -m "15_columns_share_post" && git push --set-upstream origin 15_columns_share_post
 
 # act1_screens_carapia
 
-# Task 14
+# Task 15
 
 ## Screenshot
 
-![14_exercise14](screenshots/exercise14.png)
+![15_exercise15](screenshots/exercise15.png)
 
--- lib/main.dart
+++ lib/main.dart
@@ -11,118 +11,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
-  color: Colors.grey[300],
-  width: double.infinity,
-  height: double.infinity,
-
-  child: Row( //Begin Row 1
-
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    crossAxisAlignment: CrossAxisAlignment.center,
-
-    children: [ // Contents of Row 1
-
-      Column( // Begin Col 1 in Row 1
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-        children: [ // Contents of Col1 in Row 1
-          Container( //Facebook icon and text
-            height: 100,
-            width: 100,
-            color: Colors.white,
+        padding: EdgeInsets.all(16.0),
+          
+          child: Container(
+            
+            
             child: Column(
-                    children: [
-                      CircleAvatar(
-                        child: Image.asset('images/facebook.png'),
-                          ),
-                      Text("Facebook", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),), 
-               //End of Content Col 1 Row 1 (facebook and text)
-                        ],
-                  ),
-            ),
-
-            Container( //Contents of Col1 in Row 2
-              height: 100,
-              width: 100,
-              color: Colors.white,
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    child: Image.asset('images/instagram.jpg'),
-                  ),
-                  Text("Instagram",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                //End of Content in Col 1 Row 2 (instagram and text)
-                ],//children container
-                ),
-            ),
-            Container(
-              height: 100,
-              width: 100,
-              color:Colors.white,
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    child: Image.asset('images/telegram.png'),
-                  ),
-                  Text('Telegram',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                ],
-                ),
-            )
-        ], //end children column
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.white,
-            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CircleAvatar(
-                  child: Image.asset('images/twitter.png'),
-                ),
-                Text('Twitter',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ],
-              ),
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.white,
-            child: Column(
-              children: [
-                CircleAvatar(
-                  child: Image.asset('images/youtube.png'),
-                ),
-                Text('Youtube',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              ],
+                
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: Image.asset('images/rain.jpg',fit: BoxFit.cover,)
+                    ),
 
-            ),
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.white,
-            child: Column(
-              children: [
-                CircleAvatar(
-                  child: Image.asset('images/star.png'),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      
+                      children: [
+                        Text('Heavy Rain',style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold) ,),
+                        Icon(Icons.favorite_border),
+                       
+                        SizedBox(height: 8),
+                       
+                        
+                      ],
+                    )
+                  ],
                 ),
-                Text('Rate the App',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                 Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("5 min ago"),
+                            SizedBox(height: 8),
+                          ],
+                        ),
+                 Row(
+                   mainAxisAlignment: MainAxisAlignment.start,
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     Text("njghkjdfnggnojnjmmmfsls"),
+                   ],
+                 ),
+                 ElevatedButton(onPressed:(){} , child:Text("Share"), ),       
+                Container(width: 20,),
+                
+               
               ],
-              ),
-          )
-        ],
-      )
-    ], // end children row
-  ),
-      ),
-        )
-  
-    );
+            ),
+          )),
+    ));
   }
 }
