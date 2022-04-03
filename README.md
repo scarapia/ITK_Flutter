0606ea8575504d93060a90113b7551100d4c0d19
scarapia <s.carapia@hotmail.com>
2022-04-03 01:15:01 -0600

    17_social_post_media_2

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-git branch 16_social_post_media && git checkout 16_social_post_media && git add . && git commit -m "16_social_post_media" && git push --set-upstream origin 16_social_post_media
+git branch 17_social_post_media_2 && git checkout 17_social_post_media_2 && git add . && git commit -m "17_social_post_media_2" && git push --set-upstream origin 17_social_post_media_2
 
 # act1_screens_carapia
 
-# Task 16
+# Task 17
 
 ## Screenshot
 
-![16_exercise16](screenshots/exercise16.png)
+![17_exercise17](screenshots/exercise17.png)
 
--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,25 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         body:  Center(
-  child: Container(
-    color: Colors.white,
-    width: double.infinity,
-    height: 308,
-    margin: EdgeInsets.all(20),
-    child: Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(12.0),
-          child: Row(
-            children: [
-              CircleAvatar(
+          
+          
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+          height: 200,
+          width: double.infinity,
+          child: Image.network(
+              "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+            fit: BoxFit.cover,
+          ),
+      ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                   CircleAvatar(
                 backgroundImage: NetworkImage(
                     "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"),
                 radius: 20,
@@ -42,49 +49,13 @@ class MyApp extends StatelessWidget {
                     ],
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 8.0,
-              ),
-              Icon(Icons.more_vert),
-            ],
-          ),
-        ),
-        Container(
-          height: 200,
-          width: double.infinity,
-          child: Image.network(
-              "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-            fit: BoxFit.cover,
-          ),
-        ),
-        SizedBox(height: 8),
-        Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                child: Row(
-                  children: [Icon(Icons.thumb_up), Text("Like")],
-                ),
-              ),
-              Container(
-                child: Row(
-                  children: [Icon(Icons.comment), Text("Comment")],
-                ),
-              ),
-              Container(
-                child: Row(
-                  children: [Icon(Icons.share), Text("Share")],
-                ),
               )
-            ],
-          ),
+              ],
+            )
+          ],
+        ), 
+      
         ),
-      ],
-    ),
-  ),
-),
       ),
     );
   }
