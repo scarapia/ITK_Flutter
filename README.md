4009d3ba491f169b038c903e25d68c1d6c56057e
scarapia <s.carapia@hotmail.com>
2022-04-03 01:57:42 -0600

    18_login_background

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-git branch 17_social_post_media_2 && git checkout 17_social_post_media_2 && git add . && git commit -m "17_social_post_media_2" && git push --set-upstream origin 17_social_post_media_2
+git branch 18_login_background && git checkout 18_login_background && git add . && git commit -m "18_login_background" && git push --set-upstream origin 18_login_background
 
 # act1_screens_carapia
 
-# Task 17
+# Task 18
 
 ## Screenshot
 
-![17_exercise17](screenshots/exercise17.png)
+![18_exercise18](screenshots/exercise18.png)
 
--- lib/main.dart
+++ lib/main.dart
@@ -12,51 +12,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        body:  Center(
-          
-          
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-          height: 200,
+        body: Container(
+             height: double.infinity,
           width: double.infinity,
-          child: Image.network(
-              "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
-            fit: BoxFit.cover,
-          ),
-      ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+          child: Container(
+            
+            height: 500,
+            width: 500,
+            
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
+              
               children: [
-                   CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"),
-                radius: 20,
-              ),
-              SizedBox(
-                width: 8.0,
-              ),
-              Expanded(
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Sandra S"),
-                      Text("5 mins ago"),
-                    ],
-                  ),
+                FittedBox(
+                  child: Image.network(
+                    "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+                    fit:BoxFit.fill),
+                  
                 ),
-              )
+                
+               
+                
+                    
+                     SignInButton(
+                        Buttons.Google,
+                        onPressed: () {},
+                      ),
+                    ElevatedButton(
+                        child:  
+                          Text("Continue with Facebook"),
+                          onPressed: () {},
+                  ),
+                    Text("You agree to the terms and conditions"),
+              
+                  
+                
+                
               ],
-            )
-          ],
-        ), 
-      
+            ),
+              
         ),
-      ),
-    );
+      
+          ),
+          ),
+      );
   }
 }
